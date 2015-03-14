78744598df9b02623c569e7b165099e5f059cba8
brucebeisel <brucebeisel@gmail.com>
2015-03-14 16:39:39 -0700

    Changed to Lesser GPL and added Gamma correction table.

--- src/main/java/com/bdb/piglow4j/PiGlowLED.java
+++ src/main/java/com/bdb/piglow4j/PiGlowLED.java
@@ -1,17 +1,17 @@
-/*
+/* 
  * Copyright (C) 2015 Bruce Beisel
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.bdb.piglow4j;
@@ -38,6 +38,26 @@ public class PiGlowLED {
     private static final Map<PiGlowArm,List<PiGlowLED>> arms = new HashMap<>();
     private static final Map<PiGlowColor,List<PiGlowLED>> colors = new HashMap<>();
     private static final List<PiGlowLED> ledList = new ArrayList<>();
+    private static boolean performGammaCorrection = true;
+    // Gamma correction table courtesy of Ben Lebherz
+    private static final int GAMMA_CORRECTION_TABLE[] = {
+          0,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,
+          1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,
+          2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,   2,
+          2,   2,   2,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,
+          4,   4,   4,   4,   4,   4,   4,   4,   4,   4,   4,   5,   5,   5,   5,   5,
+          5,   5,   5,   6,   6,   6,   6,   6,   6,   6,   7,   7,   7,   7,   7,   7,
+          8,   8,   8,   8,   8,   8,   9,   9,   9,   9,  10,  10,  10,  10,  10,  11,
+         11,  11,  11,  12,  12,  12,  13,  13,  13,  13,  14,  14,  14,  15,  15,  15,
+         16,  16,  16,  17,  17,  18,  18,  18,  19,  19,  20,  20,  20,  21,  21,  22,
+         22,  23,  23,  24,  24,  25,  26,  26,  27,  27,  28,  29,  29,  30,  31,  31,
+         32,  33,  33,  34,  35,  36,  36,  37,  38,  39,  40,  41,  42,  42,  43,  44,
+         45,  46,  47,  48,  50,  51,  52,  53,  54,  55,  57,  58,  59,  60,  62,  63,
+         64,  66,  67,  69,  70,  72,  74,  75,  77,  79,  80,  82,  84,  86,  88,  90,
+         91,  94,  96,  98, 100, 102, 104, 107, 109, 111, 114, 116, 119, 122, 124, 127,
+        130, 133, 136, 139, 142, 145, 148, 151, 155, 158, 161, 165, 169, 172, 176, 180,
+        184, 188, 192, 196, 201, 205, 210, 214, 219, 224, 229, 234, 239, 244, 250, 255
+    };
 
     static {
         //
@@ -126,6 +146,10 @@ public class PiGlowLED {
         return Collections.unmodifiableList(ledList);
     }
 
+    public static void setGammaCorrectionMode(boolean enabled) {
+        performGammaCorrection = enabled;
+    }
+
     private static int LedIdentifier(PiGlowArm arm, PiGlowColor color) {
         return arm.ordinal() << 8 | color.ordinal();
     }
@@ -147,9 +171,16 @@ public class PiGlowLED {
      * Set the intensity of the LED. Note that this will not take effect until updateLEDs() is called.
      * 
      * @param value The new intensity value
+     * @throws IllegalArgumentException Thrown if the intensity is out of range
      */
-    public void setIntensity(int value) {
-        intensity = value;
+    public void setIntensity(int value) throws IllegalArgumentException {
+        if (value < MIN_INTENSITY || value > MAX_INTENSITY)
+            throw new IllegalArgumentException("Intensity must be from " + MIN_INTENSITY + " to " + MAX_INTENSITY);
+
+        if (performGammaCorrection)
+            intensity = GAMMA_CORRECTION_TABLE[value];
+        else
+            intensity = value;
     }
 
     /**
